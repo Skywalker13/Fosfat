2e67995385eb34c3fb4b20ed9c4c7c6173d89a39
Mathieu Schroeter <mathieu@schroetersa.ch>
2006-12-26 16:07:49 +0000

    simplify fosread for use new high level functions

--- fosread/fosread.c
+++ fosread/fosread.c
@@ -26,81 +26,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> /* strcmp strcpy */
-#include <ctype.h>  /* tolower */
 
 #include "fosfat.h"
 
 #define VERSION     "0.0.1"
 
-#define YEAR2K      0x73
-
 typedef struct ginfo {
     char name[FOSFAT_NAMELGT];
 } s_global_info;
-
-/** Change a string in lower case.
- * @param data the string
- * @return a pointer on this string
- */
-char *lc(char *data) {
-    int i;
-
-    for (i = 0; data[i] != '\0'; i++)
-        data[i] = tolower(data[i]);
-    return data;
-}
 
 /** Get info from the block 0.
  * @param dev pointer on the device
  * @return info
  */
 s_global_info *get_ginfo(FOSFAT_DEV *dev) {
-    s_fosfat_b0 *block0;
     s_global_info *ginfo;
+    char *name;
 
-    if ((block0 = fosfat_read_b0(dev, FOSFAT_BLOCK0))) {
+    if ((name = fosfat_diskname(dev))) {
         ginfo = (s_global_info *)malloc(sizeof(s_global_info));
-        strcpy(ginfo->name, block0->nlo);
-        ginfo->name[FOSFAT_NAMELGT - 1] = '\0';
-        lc(ginfo->name);
+        strcpy(ginfo->name, name);
+        free(name);
     }
     else {
-        printf("ERROR: I can't read the block 0!\n");
+        printf("ERROR: I can't read the name of this disk!\n");
         ginfo = NULL;
     }
-
-    free(block0);
     return ginfo;
 }
 
 /** Print date and hour.
  * @param dh date
  */
-void print_date(unsigned char *dh) {
-    printf(" %2i%02X-%02X-%02X %02X:%02X", dh[2] < YEAR2K ? 20 : 19, dh[2], dh[1], dh[0], dh[3], dh[4]);
+void print_date(s_fosfat_time *time) {
+    printf(" %4i-%02i-%02i %02i:%02i", time->year, time->month, time->day, time->hour, time->minute);
 }
 
 /** Print a file in the list.
  * @param file BLF of a file
  */
-void print_file(s_fosfat_blf *file) {
-    int i;
+void print_file(const char *location, s_fosfat_listdir *file) {
+    char *path;
     char filename[FOSFAT_NAMELGT];
 
-    if (fosfat_isdir(file))
+    path = (char *)malloc(sizeof(location) + sizeof(file->name) + sizeof(char));
+
+    if (*location == '\0' || !strcmp(location, "/"))
+        strcpy(path, file->name);
+    else
+        sprintf(path, "%s/%s", location, file->name);
+
+    if (file->att.isdir)
         snprintf(filename, strrchr(file->name, '.') - file->name + 1, "%s", file->name);
     else
         strncpy(filename, file->name, FOSFAT_NAMELGT - 1);
     filename[FOSFAT_NAMELGT - 1] = '\0';
-    lc(filename);
-    printf("%c%c%c%c %10li", fosfat_isdir(file) ? 'd' : '-',
-                           fosfat_isvisible(file) ? '-' : 'h',
-                           fosfat_issystem(file) ? 's' : '-',
-                           fosfat_isencoded(file) ? 'e' : '-',
-                           c2l(file->lgf, sizeof(file->lgf)));
-    for (i = 0; i < 3; i++)
-        print_date(file->cd + i * 6);
+
+    printf("%c%c%c %11i", file->att.isdir ? 'd' : '-',
+                          file->att.isvisible ? '-' : 'h',
+                          file->att.isencoded ? 'e' : '-',
+                          file->size);
+    print_date(&file->time_c);
+    print_date(&file->time_w);
+    print_date(&file->time_r);
     printf(" %s\n", filename);
+    free(path);
 }
 
 /** List the content of a directory.
@@ -109,40 +99,21 @@ void print_file(s_fosfat_blf *file) {
  * @return true if it is ok
  */
 int list_dir(FOSFAT_DEV *dev, const char *path) {
-    int i, empty = 1;
-    s_fosfat_bd *dir;
-    s_fosfat_bl *files;
+    s_fosfat_listdir *files, *first_file;
 
-    if ((dir = fosfat_search_insys(dev, path, eSBD))) {
-        if (fosfat_p_isdir(dev, path)) {
-            files = dir->first_bl;
-            printf("path: ");
-            if (fosfat_isbdsys(dev, dir))
-                printf("/\n");
-            else
-                printf("%s\n", path);
-            printf("\n           size creation         last change      last view        filename\n");
-            printf("           ---- --------         -----------      ---------        --------\n");
-            do {
-                /* Check all file in the BL */
-                for (i = 0; i < 4; i++) {
-                    if (fosfat_isopenexm(&files->file[i]) && strlen(files->file[i].name) > 0) {
-                        print_file(&files->file[i]);
-                        empty = 0;
-                    }
-                }
-            } while ((files = files->next_bl));
-            if (!empty)
-                printf("\nd:directory  h:hidden  s:system  e:encoded\n");
-            fosfat_free_dir(dir);
-        }
-        else {
-            printf("ERROR: this path is not a directory!\n");
-            return 0;
-        }
+    if ((files = fosfat_list_dir(dev, path))) {
+        first_file = files;
+        printf("path: %s\n", path);
+        printf("\n           size creation         last change      last view        filename\n");
+        printf("           ---- --------         -----------      ---------        --------\n");
+        do {
+            print_file(path, files);
+        } while ((files = files->next_file));
+        printf("\nd:directory  h:hidden  e:encoded\n");
+        fosfat_free_listdir(first_file);
     }
     else {
-        printf("ERROR: I can't read the SYS_LIST!\n");
+        printf("ERROR: I can't found this path or this directory is empty!\n");
         return 0;
     }
     return 1;
@@ -158,36 +129,19 @@ int list_dir(FOSFAT_DEV *dev, const char *path) {
 int get_file(FOSFAT_DEV *dev, const char *path, const char *dst) {
     int res = 0;
     char *new_file;
-    s_fosfat_bd *syslist, *file;
-    s_fosfat_bl *files;
 
-    new_file = strdup(dst);
-
-    if ((syslist = fosfat_read_dir(dev, FOSFAT_SYSLIST))) {
-        files = syslist->first_bl;
-        file = fosfat_search_bdlf(dev, path, files, eSBD);
-        if (file && !file->first_bl) {
-            if (!strcasecmp(new_file, "./"))
-                new_file = strdup(file->name);
-            lc(new_file);
-            printf("File \"%s\" is copying ...\n", path);
-            if (fosfat_get_file(dev, file, lc(new_file), 1))
-                res = 1;
-            if (res)
-                printf("Okay..");
-            printf("\n");
-        }
-        else
-            printf("ERROR: I can't found the file!\n");
-        /* Freed variables */
-        fosfat_free_dir(syslist);
-        if (file && !file->first_bl)
-            fosfat_free_file(file);
-        else if (file)
-            fosfat_free_dir(file);
+    if (!strcasecmp(dst, "./"))
+        new_file = strdup((strrchr(path, '/') ? strrchr(path, '/') + 1 : path));
+    else
+        new_file = strdup(dst);
+    printf("File \"%s\" is copying ...\n", path);
+    if (fosfat_get_file(dev, path, new_file, 1)) {
+        res = 1;
+        printf("Okay..\n");
     }
     else
-        printf("ERROR: I can't read the SYS_LIST!\n");
+        printf("ERROR: I can't copy the file!\n");
+
     free(new_file);
     return res;
 }
