e04b87ab6a7bf8b290cec00393ab70f7deaefddd
Mathieu Schroeter <mathieu@schroetersa.ch>
2006-12-26 15:48:19 +0000

    now, a lot of definitions are private

--- libfosfat/fosfat.h
+++ libfosfat/fosfat.h
@@ -43,19 +43,6 @@ typedef enum disk_type {
     eHD                          //!< Hard Disk
 } e_fosfat_disk;
 
-/** Search type */
-typedef enum search_type {
-    eSBD,                        //!< Search BD
-    eSBLF                        //!< Search BL File
-} e_fosfat_search;
-
-/** Data Block (256 bytes) */
-typedef struct block_data {
-    unsigned char data[256];     //!< Data
-    /* Linked list */
-    struct block_data *next_data;
-} s_fosfat_data;
-
 /** Block 0 (256 bytes) */
 typedef struct block_0 {
     unsigned char sys[44];       //!< SYSTEM folder
@@ -68,58 +55,6 @@ typedef struct block_0 {
     unsigned char newchk[4];     //!< New CHK value
     unsigned char reserve[10];   //!< Unused
 } s_fosfat_b0;
-
-/** File in a Block List (60 bytes) */
-typedef struct block_listf {
-    char name[16];               //!< Filename
-    unsigned char typ;           //!< Filetype
-    unsigned char ope;           //!< Open counter
-    unsigned char att[4];        //!< Attributes
-    unsigned char lg[4];         //!< Length in blocks
-    unsigned char lgb[2];        //!< Number of bytes in the last block
-    unsigned char cd[3];         //!< Date of Creation
-    unsigned char ch[3];         //!< Hour of Creation
-    unsigned char wd[3];         //!< Date of the last Write
-    unsigned char wh[3];         //!< Hour of the last Write
-    unsigned char rd[3];         //!< Date of the last Read
-    unsigned char rh[3];         //!< Hour of the last Read
-    unsigned char secid[3];      //!< Security ID
-    unsigned char clope;         //!< Open mode before CLOSE
-    unsigned char pt[4];         //!< Pointer on the BD
-    unsigned char lgf[4];        //!< File size in bytes
-    unsigned char code[2];       //!< Code control
-} s_fosfat_blf;
-
-/** Block List (256 bytes) */
-typedef struct block_list {
-    s_fosfat_blf file[4];        //!< 4 BL files (240 bytes)
-    unsigned char next[4];       //!< Next BL
-    unsigned char chk[4];        //!< Check control
-    unsigned char prev[4];       //!< Previous BL
-    unsigned char reserve[4];    //!< Unused
-    /* Linked list */
-    struct block_list *next_bl;
-} s_fosfat_bl;
-
-/** Block Description (256 bytes) */
-typedef struct block_desc {
-    unsigned char next[4];       //!< Next BD
-    unsigned char prev[4];       //!< Previous BD
-    unsigned char npt[2];        //!< Number of tranches in the BD
-    unsigned char pts[42][4];    //!< Pointers on the tranches (max 42 tranches)
-    char name[16];               //!< Filename
-    unsigned char nbs[42];       //!< Length (in blocks) of each tranches (1 byte)
-    unsigned char reserve[4];    //!< Unused
-    unsigned char lst[2];        //!< Number of byte in the last tranche
-    unsigned char hac[2];        //!< Hashing function if LIST
-    unsigned char nbl[2];        //!< Number of BL used if LIST
-    unsigned char chk[4];        //!< Check control
-    unsigned char off[4];        //!< Offset (in blocks) of all previous BD
-    unsigned char free[2];       //!< Unused
-    /* Linked list */
-    struct block_desc *next_bd;
-    struct block_list *first_bl;
-} s_fosfat_bd;
 
 /** Time */
 typedef struct time {
