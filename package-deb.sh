69aee13476fa8df2c64a47730bcdd41513027e93
Mathieu Schroeter <mathieu@schroetersa.ch>
2006-12-30 17:22:24 +0000

    add debian rules for create a package

---
+++ package-deb.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+. ./version
+
+DIR=fosfat-$VERSION.$PATCHLEVEL.$SUBLEVEL
+PACKAGE=package-deb
+
+rm -rf "$PACKAGE"
+mkdir -p "$PACKAGE/$DIR"
+
+cp -pPR Makefile README COPYING version ChangeLog package-deb.sh configure fosmount fosread libfosfat debian "$PACKAGE/$DIR"
+find "$PACKAGE/$DIR" \( -name .svn -or -name .depend -or -name '*.o' \) -exec rm -rf '{}' \; 2>/dev/null
+
+cd "$PACKAGE/$DIR"
+dpkg-buildpackage -rfakeroot -us -uc
+
+cd ..
+rm -rf "$DIR"
