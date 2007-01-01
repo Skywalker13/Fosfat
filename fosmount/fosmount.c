1f7f9757dc009976906ec13d948d5275f994eb46
Mathieu Schroeter <mathieu@schroetersa.ch>
2007-01-01 10:04:46 +0000

    update copyright for 2007

--- fosmount/fosmount.c
+++ fosmount/fosmount.c
@@ -1,6 +1,6 @@
 /*
  * FOS fosmount: FUSE extension in read-only for Smaky file system
- * Copyright (C) 2006 Mathieu Schroeter <mathieu.schroeter@gamesover.ch>
+ * Copyright (C) 2006-2007 Mathieu Schroeter <mathieu.schroeter@gamesover.ch>
  *
  * Thanks to Pierre Arnaud <pierre.arnaud@opac.ch>
  *           for his help and the documentation
