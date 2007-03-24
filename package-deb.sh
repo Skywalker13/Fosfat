e9c656d7ed1eb91611d682664c8a63f8455ef748
Mathieu Schroeter <mathieu@schroetersa.ch>
2007-03-24 19:05:47 +0000

    fix package-deb script

--- package-deb.sh
+++ package-deb.sh
@@ -8,7 +8,7 @@ PACKAGE=package-deb
 rm -rf "$PACKAGE"
 mkdir -p "$PACKAGE/$DIR"
 
-cp -pPR Makefile README COPYING version ChangeLog package-deb.sh configure fosmount fosread libfosfat debian "$PACKAGE/$DIR"
+cp -pPR Makefile README COPYING VERSION TODO ChangeLog package-deb.sh configure fosmount tools libfosfat debian "$PACKAGE/$DIR"
 find "$PACKAGE/$DIR" \( -name .svn -or -name .depend -or -name '*.o' \) -exec rm -rf '{}' \; 2>/dev/null
 cd "$PACKAGE"
 tar -czf "fosfat_$VERSION.$PATCHLEVEL.$SUBLEVEL.orig.tar.gz" "$DIR"
