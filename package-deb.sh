e7c272f71d32fd9ee65c1fa239bec9cd9037b8ba
Mathieu Schroeter <mathieu@schroetersa.ch>
2007-03-25 18:55:24 +0000

    change deb builder for CDBS

--- package-deb.sh
+++ package-deb.sh
@@ -22,22 +22,21 @@ cp -pPR Makefile \
 find "$PACKAGE/$DIR" \( -name .svn -or \
                         -name .depend* -or \
                         -name '*.o' -or \
-                        -name '*.a' \
+                        -name '*.a' -or \
+                        -name '*~' \
                      \) -exec rm -rf '{}' \; 2>/dev/null
 find "$PACKAGE/$DIR" \( -name 'fosread' -or \
                         -name 'smascii' -or \
                         -name 'fosmount' \
                      \) -exec rm -f '{}' \; 2>/dev/null
 
-echo "cd $PACKAGE && sudo pbuilder build *dsc" 1> gen-deb.sh
-chmod a+x gen-deb.sh
 cd "$PACKAGE"
 tar -czf "fosfat_$VERSION.$PATCHLEVEL.$SUBLEVEL.orig.tar.gz" "$DIR"
 
 cp -pPR ../debian "$DIR"
-find "$DIR" \( -name .svn \) -exec rm -rf '{}' \; 2>/dev/null
+find "$DIR" \( -name .svn or -name *~ \) -exec rm -rf '{}' \; 2>/dev/null
 cd "$DIR"
-debuild -S -sa
+debuild -us -uc
 
 cd ..
 rm -rf "$DIR"
