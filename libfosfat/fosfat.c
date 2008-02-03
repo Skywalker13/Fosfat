1201d267f1d464b40703980699ac00ce8080b789
Mathieu Schroeter <mathieu@schroetersa.ch>
2008-02-03 22:20:58 +0000

    rewrite conditions (no fonctionality change) (10)

--- libfosfat/fosfat.c
+++ libfosfat/fosfat.c
@@ -1528,13 +1528,14 @@ fosfat_islink (fosfat_t *fosfat, const char *location)
   int res = 0;
   fosfat_blf_t *entry;
 
-  if (fosfat && location) {
-    if ((entry = fosfat_search_insys (fosfat, location, eSBLF))) {
-      if (fosfat_in_islink (entry))
-        res = 1;
+  if (!fosfat || !location)
+    return 0;
 
-      free (entry);
-    }
+  if ((entry = fosfat_search_insys (fosfat, location, eSBLF))) {
+    if (fosfat_in_islink (entry))
+      res = 1;
+
+    free (entry);
   }
 
   return res;
