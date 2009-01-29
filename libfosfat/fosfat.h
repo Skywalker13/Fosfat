40e8d41ce4f83a742116e93ffdc522fa42a562da
Mathieu Schroeter <mathieu@schroetersa.ch>
2009-01-29 16:46:45 +0000

    use doxygen in the public header

--- libfosfat/fosfat.h
+++ libfosfat/fosfat.h
@@ -30,15 +30,15 @@
 
 #define F_UNDELETE      (1 << 0)
 
-/** Disk types */
+/** Disk types. */
 typedef enum disk_type {
-  FOSFAT_FD,                   /*!< Floppy Disk           */
-  FOSFAT_HD,                   /*!< Hard Disk             */
-  FOSFAT_AD,                   /*!< Auto Detection        */
-  FOSFAT_ED                    /*!< Auto Detection fails  */
+  FOSFAT_FD,                   /*!< Floppy Disk.          */
+  FOSFAT_HD,                   /*!< Hard Disk.            */
+  FOSFAT_AD,                   /*!< Auto Detection.       */
+  FOSFAT_ED                    /*!< Auto Detection fails. */
 } fosfat_disk_t;
 
-/** Time */
+/** Time. */
 typedef struct time_s {
   short int year;
   short int month;
@@ -48,7 +48,7 @@ typedef struct time_s {
   short int second;
 } fosfat_time_t;
 
-/** Attributes for a file (or dir) */
+/** Attributes for a file (or dir).*/
 typedef struct att_s {
   int isdir     : 1;
   int isvisible : 1;
@@ -57,23 +57,23 @@ typedef struct att_s {
   int isdel     : 1;
 } fosfat_att_t;
 
-/** List of files in a directory */
+/** List of files in a directory. */
 typedef struct file_info_s {
-  char name[FOSFAT_NAMELGT];  /*!< File name              */
-  int size;                   /*!< File size              */
-  fosfat_att_t att;           /*!< File attributes        */
-  fosfat_time_t time_c;       /*!< Creation date          */
-  fosfat_time_t time_w;       /*!< Writing date           */
-  fosfat_time_t time_r;       /*!< Use date               */
+  char name[FOSFAT_NAMELGT];  /*!< File name.             */
+  int size;                   /*!< File size.             */
+  fosfat_att_t att;           /*!< File attributes.       */
+  fosfat_time_t time_c;       /*!< Creation date.         */
+  fosfat_time_t time_w;       /*!< Writing date.          */
+  fosfat_time_t time_r;       /*!< Use date.              */
   /* Linked list */
   struct file_info_s *next_file;
 } fosfat_file_t;
 
-/* fosfat opaque data types */
+/** Fosfat handle on a disk. */
 typedef struct fosfat_s fosfat_t;
 
 
-/*
+/**
  * Load a device compatible Smaky FOS. The device can be a file or a device.
  * But with Window$ currently only device are supported. fosfat_close() must
  * always be called to freeing the memory and close the device.
@@ -82,123 +82,163 @@ typedef struct fosfat_s fosfat_t;
  * Window$ : specify the device with 'a' for diskette, 'c' for the first hard
  *           disk, etc,...
  *
- * param dev[in]        device or location
- * param disk[in]       type of disk, use FOSFAT_AD for auto-detection
- * param flag[in]       use F_UNDELETE for load deleted files or 0 for normal
- * return NULL if error or return the disk loaded
+ * \param[in] dev        device or location.
+ * \param[in] disk       type of disk, use FOSFAT_AD for auto-detection.
+ * \param[in] flag       use F_UNDELETE for load deleted files or 0 for normal.
+ * \return NULL if error or return the disk handle.
  */
 fosfat_t *fosfat_open (const char *dev, fosfat_disk_t disk, unsigned int flag);
 
-/*
+/**
  * Free the memory and close the device properly.
  *
- * param fosfat[in]     disk loaded
+ * \param[in] fosfat     disk handle.
  */
 void fosfat_close (fosfat_t *fosfat);
 
-/*
+/**
  * By default, the internal logger is disabled. Use this function to enable
  * or disable the verbosity. The logger is enabled or disabled for all
  * devices loaded.
  *
- * param state[in]      boolean, 0 for disable the logger
+ * \param[in] state      boolean, 0 for disable the logger.
  */
 void fosfat_logger (int state);
 
-/*
+/**
  * Get the diskname of a specific disk. The pointer must be freed when
  * no longer used.
  *
- * param fosfat[in]     disk loaded
- * return NULL if error/null-string or return the name
+ * \param[in] fosfat     disk handle.
+ * \return NULL if error/null-string or return the name.
  */
 char *fosfat_diskname (fosfat_t *fosfat);
 
-/*
+/**
  * Get file/dir list of a directory in a linked list. When the list is no
  * longer used, fosfat_free_listdir() must always be called to freeing
  * the memory.
  *
- * param fosfat[in]     disk loaded
- * param location[in]   directory to list
- * return NULL if error or return the first file in the directory
+ * \param[in] fosfat     disk handle.
+ * \param[in] location   directory to list.
+ * \return NULL if error or return the first file in the directory.
  */
 fosfat_file_t *fosfat_list_dir (fosfat_t *fosfat, const char *location);
 
-/*
+/**
  * Free the memory for an linked list created by fosfat_list_dir().
  *
- * param var[in]        first file of the linked list
+ * \param[in] var        first file of the linked list.
  */
 void fosfat_free_listdir (fosfat_file_t *var);
 
-/*
+/**
  * Get boolean information on a specific file or folder spedified with
  * a location.
  *
- * isdir     : test if the location is a directory
- * islink    : test if the location is a soft-link
- * isvisible : test if the location is not hidden
- * isencoded : test if the location is encoded
- * isopenexm : test if the location is 'open exclusif' and 'multiple'
+ * Test if the location is a directory.
  *
- * param fosfat[in]     disk loaded
- * param location[in]   file or directory to test
- * return a boolean, 0 for false
+ * \param[in] fosfat     disk handle.
+ * \param[in] location   file or directory to test.
+ * \return a boolean, 0 for false.
  */
 int fosfat_isdir (fosfat_t *fosfat, const char *location);
+
+/**
+ * Get boolean information on a specific file or folder spedified with
+ * a location.
+ *
+ * Test if the location is a soft-link.
+ *
+ * \param[in] fosfat     disk handle.
+ * \param[in] location   file or directory to test.
+ * \return a boolean, 0 for false.
+ */
 int fosfat_islink (fosfat_t *fosfat, const char *location);
+
+/**
+ * Get boolean information on a specific file or folder spedified with
+ * a location.
+ *
+ * Test if the location is not hidden.
+ *
+ * \param[in] fosfat     disk handle.
+ * \param[in] location   file or directory to test.
+ * \return a boolean, 0 for false.
+ */
 int fosfat_isvisible (fosfat_t *fosfat, const char *location);
+
+/**
+ * Get boolean information on a specific file or folder spedified with
+ * a location.
+ *
+ * Test if the location is encoded.
+ *
+ * \param[in] fosfat     disk handle.
+ * \param[in] location   file or directory to test.
+ * \return a boolean, 0 for false.
+ */
 int fosfat_isencoded (fosfat_t *fosfat, const char *location);
+
+/**
+ * Get boolean information on a specific file or folder spedified with
+ * a location.
+ *
+ * Test if the location is 'open exclusif' and 'multiple'.
+ *
+ * \param[in] fosfat     disk handle.
+ * \param[in] location   file or directory to test.
+ * \return a boolean, 0 for false.
+ */
 int fosfat_isopenexm (fosfat_t *fosfat, const char *location);
 
-/*
+/**
  * Get some informations on a file or a directory. File size, attributes,
  * creation date, writing date and use date. Look fosfat_file_t structure
  * for more informations. The pointer must be freed when no longer used.
  *
- * param fosfat[in]     disk loaded
- * param location[in]   file or directory where get informations
- * return NULL if error or return the file structure
+ * \param[in] fosfat     disk handle.
+ * \param[in] location   file or directory where get informations.
+ * \return NULL if error or return the file structure.
  */
 fosfat_file_t *fosfat_get_stat (fosfat_t *fosfat, const char *location);
 
-/*
+/**
  * If the location is a soft-link, then this function will return the
  * location of the target. The pointer must be freed when no longer used.
  *
- * param fosfat[in]     disk loaded
- * param location[in]   soft-link
- * return NULL if error/null-string or return the location
+ * \param[in] fosfat     disk handle.
+ * \param[in] location   soft-link.
+ * \return NULL if error/null-string or return the location.
  */
 char *fosfat_symlink (fosfat_t *fosfat, const char *location);
 
-/*
+/**
  * Get a file from a location and put this on the user hard drive. You can't
  * get a directory with this function, you must recursively list directory
  * with fosfat_list_dir() and use fosfat_get_file with each file entry.
  *
- * param fosfat[in]     disk loaded
- * param src[in]        source location on the FOS disk
- * param dst[in]        destination location on the user hard drive
- * param output[in]     boolean for print copy progression in the console
- * return a boolean, 0 for error
+ * \param[in] fosfat     disk handle.
+ * \param[in] src        source location on the FOS disk.
+ * \param[in] dst        destination location on the user hard drive.
+ * \param[in] output     boolean for print copy progression in the console.
+ * \return a boolean, 0 for error.
  */
-int fosfat_get_file (fosfat_t *fosfat, const char *src,
-                     const char *dst, int output);
+int fosfat_get_file (fosfat_t *fosfat,
+                     const char *src, const char *dst, int output);
 
-/*
+/**
  * Get a buffer of a file. Usefull for get only a part of a file without
  * save anything on the user hard drive. You can get all the data if you
  * prefer. The pointer must be freed when no longer used.
  *
- * param fosfat[in]     disk loaded
- * param path[in]       file where get data
- * param offset[in]     from where (in bytes) in the data
- * param size[in]       how many bytes
- * return NULL if error or return the buffer
+ * \param[in] fosfat     disk handle.
+ * \param[in] path       file where get data.
+ * \param[in] offset     from where (in bytes) in the data.
+ * \param[in] size       how many bytes.
+ * \return NULL if error or return the buffer.
  */
-char *fosfat_get_buffer (fosfat_t *fosfat, const char *path,
-                         int offset, int size);
+char *fosfat_get_buffer (fosfat_t *fosfat,
+                         const char *path, int offset, int size);
 
 #endif /* FOSFAT_H */
